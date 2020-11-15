aff869d183ca71c327712cdd332f623de7936f38
xTraOV <vazhakobalava@gmail.com>
2020-11-15 12:40:39 +0400

    asd

--- card game war/initial.py
+++ card game war/initial.py
@@ -22,10 +22,24 @@ class Deck:
         for suit in suits:
             for rank in ranks:
                 created_card = Card(suit,rank)
-                self.all_cards.append(created_card)
-        
+                self.all_cards.append(created_card)    
     def shuffle(self):
         shuffle(self.all_cards)
-        
     def deal_one(self):
         return self.all_cards.pop()
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.all_cards = []
+    def remove_one(self):
+        return self.all_cards.pop(0)
+    def add_cards(self,new_cards0):
+        if type(new_cards)==type([]):
+            self.all_cards.extend(new_cards)
+        else:
+            self.all_cards.append(new_cards)
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+
